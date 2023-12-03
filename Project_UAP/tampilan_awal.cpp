f05d396fafde5afb8aaf7fec5a7d494a70f3809e
lia <144763130+lillia05@users.noreply.github.com>
2023-12-03 19:16:05 +0700

    nambahin fungsi

--- Project_UAP/tampilan_awal.cpp
+++ Project_UAP/tampilan_awal.cpp
@@ -3,38 +3,69 @@
 #include <iostream>
 using namespace std;
 	
+void instruction(){
+	initscr();
+	clear();
+	mvprintw(7,50,"INSTRUKSI PERMAINAN");
+	getch();
+	endwin();
+}
 
+void game(){
+	initscr();
+	clear();
+	system("go_dino");
+	getch();
+	endwin();
+}
+	
 int main(){
+	curs_set(0);
 	int nlines =5; int ncols = 32; int y0 = 1; int x0=5;
 	WINDOW *win = newwin(nlines, ncols, y0, x0);
 	system("color E0");
 	initscr();
+	
 	mvprintw(10,50,"--------------------");
 	mvprintw(11,50,"|    GO GO DINO    |");
 	mvprintw(12,50,"--------------------");
 	refresh();
-	Sleep(700);
+	Sleep(600);
 	
 	mvprintw(15,50," 1. Game start     ");
 	refresh();
-	Sleep(500);
+	Sleep(400);
 
 	mvprintw(16,50," 2. Instruction    ");
 	refresh();
-	Sleep(500);
+	Sleep(400);
 	
-	mvprintw(17,50," 3. quit        ");
+	mvprintw(17,50," 3. Exit        ");
 	refresh();
-	Sleep(500);
+	Sleep(400);
 	
 	mvprintw(19,50," select option :  ");
 	refresh();
-	Sleep(500);
-		
-	wrefresh(win);
-	int ch = wgetch(win);
-	delwin(win);
-		
-	getch();
+	Sleep(400);
+	
+	while (true)
+	{
+		char jawab = getch();
+		if (jawab == '1')
+		{
+//			game();
+			break;
+		}
+		else if (jawab == '2')
+		{
+			instruction();
+			break;
+		}
+		else if (jawab == '3')
+		{
+			endwin();
+			exit(0);
+		}
+	}
 	endwin();
 }
