c3551ccad25791ac97e5692f8533dd79ab7f18c0
lia <144763130+lillia05@users.noreply.github.com>
2023-12-04 22:28:28 +0700

    nambahin level game

--- Project_UAP/game.cpp
+++ Project_UAP/game.cpp
@@ -11,6 +11,56 @@ void instruction(){
 	endwin();
 }
 
+void game_start(){
+	initscr();
+	clear();
+	while (true)
+	{
+		mvprintw(8,45," _                          _");
+		mvprintw(9,45,"| |------------------------| |");
+		mvprintw(10,45,"| |      SELECT LEVEL      | |");
+		mvprintw(11,45,"| |                        | |");
+		mvprintw(12,45,"| |    1. easy             | |");
+		mvprintw(13,45,"| |    2. medium           | |");
+		mvprintw(14,45,"| |    3. hard             | |");
+		mvprintw(15,45,"| |    4. extreme          | |");
+		mvprintw(16,45,"|_|------------------------|_|");
+		refresh();
+		
+		mvprintw(19,54,"  select :  ");
+		refresh();
+		Sleep(400);
+	
+		char select = getch();
+		if (select == '1')
+		{
+			endwin();
+			system("easy");
+			break;
+		}
+		else if (select == '2')
+		{
+			endwin();
+			system("medium");
+			break;
+		}
+		else if (select == '3')
+		{
+			endwin();
+			system("hard");
+			break;
+		}
+		else if (select == '4')
+		{
+			endwin();
+			system("extreme");
+			break;
+		}
+	}
+	getch();
+	endwin();
+}
+
 	
 int main(){
 	curs_set(0);
@@ -19,11 +69,14 @@ int main(){
 	system("color E0");
 	initscr();
 	
-	mvprintw(10,50,"--------------------");
-	mvprintw(11,50,"|    GO GO DINO    |");
-	mvprintw(12,50,"--------------------");
-	refresh();
-	Sleep(600);
+//	mvprintw(7,20,"  _____  ____        _____  ____       _____ _____ _   _  ____  ");
+//	mvprintw(8,20," / ____|/ __ \      / ____|/ __ \     |  __ \_   _| \ | |/ __ \ ");
+//	mvprintw(9,20,"| |  __| |  | |    | |  __| |  | |    | |  | || | |  \| | |  | |");
+//	mvprintw(10,20,"| | |_ | |  | |    | | |_ | |  | |    | |  | || | | . ` | |  | |");
+//	mvprintw(11,20,"| |__| | |__| |    | |__| | |__| |    | |__| || |_| |\  | |__| |");
+//	mvprintw(12,20," \_____|\____/      \_____|\____/     |_____/_____|_| \_|\____/ ");
+//	refresh();
+//	Sleep(600);
 	
 	mvprintw(15,50," 1. Game start     ");
 	refresh();
@@ -46,8 +99,8 @@ int main(){
 		char option = getch();
 		if (option == '1')
 		{
-			endwin();
-			system("go_go_dino");
+			
+			game_start();
 			break;
 		}
 		else if (option == '2')
